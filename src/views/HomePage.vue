b6fcc7b1494629a5298e7d297c6f8df3a3f53d41
sedighi <zahra.sedighi1991@gmail.com>
2023-12-03 12:19:51 +0330

    Update 0.2

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,37 +9,37 @@ if (!localStorage.name) {
   route.push('/login')
 }
 
-const buttonsValues = {
-  '1/2': '0.5',
-  '1': '1',
-  '2': '2',
-  '3': '3',
-  '5': '5',
-  '8': '8',
-  '13': '13',
-  '21': '21',
-  '34': '34',
-  '55': '55',
-  '89': '86',
-  '144': '144',
-  '?': '?',
-  '∞': 'I Cant',
-  '☕': 'I Dont Want'
-}
+const buttonsValues = [
+  { key: '1/2', value: '1/2' },
+  { key: '1', value: '1' },
+  { key: '2', value: '2' },
+  { key: '3', value: '3' },
+  { key: '5', value: '5' },
+  { key: '8', value: '8' },
+  { key: '13', value: '13' },
+  { key: '21', value: '21' },
+  { key: '34', value: '34' },
+  { key: '55', value: '55' },
+  { key: '89', value: '89' },
+  { key: '144', value: '144' },
+  { key: '?', value: '?' },
+  { key: '∞', value: 'I Cant' },
+  { key: '☕', value: 'I Dont Want' }
+]
 
 async function submitPoint(num: string): Promise<void> {
   //@ts-ignore
-  await onSetPoint(localStorage.name, buttonsValues[num])
+  await onSetPoint(localStorage.name, num)
 }
 </script>
 
 <template>
   <div class="inline-grid justify-items-center grid-cols-3 gap-8 w-full content-center">
     <NumberCard
-      @click="submitPoint(index)"
-      v-for="(btn, index) in buttonsValues"
-      :key="index"
-      :text="index"
+      @click="submitPoint(btn.value)"
+      v-for="btn in buttonsValues"
+      :key="btn.key"
+      :text="btn.key"
     />
   </div>
 </template>
