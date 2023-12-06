4fb60781cef73543d492b3f5a33d8696c7718379
SaeedOmega <saeednitro2017@gmail.com>
2023-12-06 07:58:21 +0330

    change w-[60%] to w-6/10 and 999 to 9

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -18,7 +18,7 @@ function loginButton(name: string): void {
     <form @submit.prevent="loginButton(name)" class="flex gap-5 justify-center">
       <input
         type="text"
-        class="bg-transparent border-white w-[60%] shadow-sm shadow-gray-600 placeholder:text-[rgba(999,999,999,0.5)] rounded-xl border-style-solid border-2 font-sans text-xl outline-none rounded-5 p-5 text-white"
+        class="bg-transparent border-white w-6/10 shadow-sm shadow-gray-600 placeholder:text-[rgba(9,9,9,0.4)] rounded-xl border-style-solid border-2 font-sans text-xl outline-none rounded-5 p-5 text-white"
         placeholder="Type Your Name..."
         v-model="name"
         autofocus
