6c1b24d1ea96ba5a33ec198cb70dd0159d9da96a
SaeedOmega <saeednitro2017@gmail.com>
2023-12-06 07:55:20 +0330

    change name submit to login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -6,7 +6,7 @@ const router = useRouter()
 const name = ref<string>('')
 
 // #region login and save name in localStorage
-function submitButton(name: string): void {
+function loginButton(name: string): void {
   localStorage.name = name
   router.push('/')
 }
@@ -15,7 +15,7 @@ function submitButton(name: string): void {
 
 <template>
   <div class="h-screen w-screen flex flex-col justify-center items-center">
-    <form @submit.prevent="submitButton(name)" class="flex gap-5 justify-center">
+    <form @submit.prevent="loginButton(name)" class="flex gap-5 justify-center">
       <input
         type="text"
         class="bg-transparent border-white w-[60%] shadow-sm shadow-gray-600 placeholder:text-[rgba(999,999,999,0.5)] rounded-xl border-style-solid border-2 font-sans text-xl outline-none rounded-5 p-5 text-white"
@@ -26,7 +26,7 @@ function submitButton(name: string): void {
       <button
         type="button"
         class="bg-transparent rounded-xl border-white shadow-sm shadow-gray-600 border-style-solid border-2 font-sans text-xl rounded-5 p-5 text-white active:bg-[#94b6fb]"
-        @click="submitButton(name)"
+        @click="loginButton(name)"
       >
         Submit
       </button>
