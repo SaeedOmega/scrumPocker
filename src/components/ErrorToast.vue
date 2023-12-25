dd23b85dc4ed5ebb08f8c3634ca5b6f7fcce7125
SaeedOmega <saeednitro2017@gmail.com>
2023-12-25 13:32:36 +0330

    fix

--- src/components/ErrorToast.vue
+++ src/components/ErrorToast.vue
@@ -7,7 +7,7 @@ function onOkClick() {
 </script>
 
 <template>
-  <div class="absolute h-screen w-screen flex z-10 justify-center items-center">
+  <div class="absolute h-screen w-screen flex z-20 justify-center items-center">
     <div class="bg-white rounded-2xl max-w-80 p-20 gap-10 flex flex-col">
       <div>{{ message }}</div>
       <button @click="onOkClick" class="bg-yellow-200 rounded-xl">Ok</button>
