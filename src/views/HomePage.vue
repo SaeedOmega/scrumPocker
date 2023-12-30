4e1d14faffd4c3a9dc30282adf47021f2e5f7f90
SaeedOmega <saeednitro2017@gmail.com>
2023-12-30 15:22:47 +0330

    add animation to main.css

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -153,28 +153,3 @@ onUnmounted(() => {
   </div>
 </template>
 
-<style>
-.bounce-enter-active {
-  animation: bounce-in 0.5s;
-}
-.bounce-leave-active {
-  animation: bounce-out 0.5s;
-}
-
-@keyframes bounce-in {
-  0% {
-    transform: scale(0);
-  }
-  100% {
-    transform: scale(1);
-  }
-}
-@keyframes bounce-out {
-  0% {
-    transform: scale(1);
-  }
-  100% {
-    transform: scale(0);
-  }
-}
-</style>
